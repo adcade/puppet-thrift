431311fe6d62894ca00fa31011a45d42645adb08
Sebastian Otaegui <feniix@gmail.com>
2014-08-25 17:11:35 -0500

    Add fact to find the location of thrift

--- lib/facter/thrift_path.rb
+++ lib/facter/thrift_path.rb
@@ -1,21 +1,11 @@
 Facter.add(:thrift_path) do
   confine :kernel => :linux
   setcode do
-    path = which('thrift')
+    path = Facter::Util::Resolution.exec("which thrift")
     if path
       path.to_s
     else
       nil
     end
-  end
-  def which(cmd)
-    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exts.each { |ext|
-        exe = File.join(path, "#{cmd}#{ext}")
-        return exe if File.executable?(exe) && !File.directory?(exe)
-      }
-    end
-    return nil
   end
 end
